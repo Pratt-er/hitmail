49d2853b883ca23a380514fbff1ca5a6d63fdce0
Victor Nkwuo <vobinn3@gmail.com>
2023-12-30 03:07:16 +0300

    commit

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ const PORT = process.env.PORT || 3550
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/public")));
-
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 connectionToMongodb();
 
