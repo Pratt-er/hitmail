5af3b283114f73775c1b34dca3a289974378ea5c
Victor Nkwuo <vobinn3@gmail.com>
2023-12-30 03:45:42 +0300

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set('views', path.join(__dirname, 'views'))
 
 connectionToMongodb();
 
